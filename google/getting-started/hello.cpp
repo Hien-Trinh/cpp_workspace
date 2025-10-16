ccf203796a8439843591508919dece8cff789843
Hien-Trinh <hientrinhminh32@gmail.com>
2025-10-15 21:56:52 -0500

    Learning c++

---
+++ google/getting-started/hello.cpp
@@ -0,0 +1,10 @@
+// hello.cpp: David Trinh
+// Description: program prints "hello world"
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << endl;
+  return 0;
+}
